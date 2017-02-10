e0a2b8df72f35ed27a15246306670ff9366e23b8
Ruins <ruinscorocoro@gmail.com>
2017-02-11 02:03:41 +0900

    better existing file revoking

--- setup.sh
+++ setup.sh
@@ -1,24 +1,13 @@
 #!/bin/zsh
 
-DOT_FILES=( .zshrc .gitconfig .gitignore .tmux.conf .emacs.d .vimperatorrc .oh-my-zsh .subversion .zsh )
-
-DISCARD_FILES=( .emacs .emacs.el .emacs.elc )
-
-# 不要なファイルをバックアップして捨てる
-for discard in ${DISCARD_FILES[@]}
-do
-   if [ -e $HOME/$discard ]; then
-       mv -f $HOME/$discard $HOME/${discard}.old.`date +%y%m%d`
-   fi
-done
-
+DOT_FILES=( .zshrc .gitconfig .gitignore .tmux.conf .zsh )
 
 # シンボリックリンクをはる
 echo 'Make symbolic links...'
 
 for file in ${DOT_FILES[@]}
 do
-    rm -rf $HOME/$file
+    mv -f $HOME/$file $HOME/$file.bak
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
