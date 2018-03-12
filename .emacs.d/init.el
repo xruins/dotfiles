7c04032330a3eaebe6523c81b30f72edf7c472cf
Hikaru Watanabe <hikaru.watanabe@dena.com>
2018-03-12 20:24:43 +0900

    maybe I did something

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -271,9 +271,3 @@
 
 (global-set-key "\M-n" "\C-u1\C-v")
 (global-set-key "\M-p" "\C-u1\M-v")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
