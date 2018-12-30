8fcff67dcf493e00a0d45fba41c4760166f49b56
Hikaru Watanabe <hikaru.watanabe@dena.com>
2018-12-30 16:01:37 +0900

    emacs: add inits
    duplicated from https://github.com/ymotongpoo/dotfiles/blob/develop/.emacs.d/init.el.
    Thanks @ymotongpoo

---
+++ .emacs.d/inits/11_yasnippet.el
@@ -0,0 +1,18 @@
+;;;;; duplicated from https://github.com/ymotongpoo/dotfiles
+;;;;; Thanks @ymotongpoo
+
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :bind (("C-c y i" . yas-insert-snippet)
+	 ("C-c y n" . yas-new-snippet)
+	 ("C-c y v" . yas-visit-snippet-file))
+  :init
+  (add-hook 'after-init-hook 'yas-global-mode)
+  :config
+  (setq yas-snippet-dirs
+	'("~/.emacs.d/snippets/"))
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  :commands
+  (yas-minor-mode yas-global-mode))
+
---
+++ .emacs.d/inits/20_golang.el
@@ -0,0 +1,49 @@
+;;;;; duplicated from https://github.com/ymotongpoo/dotfiles
+;;;;; Thanks @ymotongpoo
+
+(use-package go-mode
+  :ensure t
+  :defer t
+  :mode
+  ("\\.go$" . go-mode)
+  :commands
+  (go-mode)
+  :bind
+  (:map go-mode-map
+	("M-." . godef-jump)
+	("C-c C-r" . go-remove-unused-imports)
+	("C-c i" . go-goto-imports)
+	("C-c d" . godoc)
+	("C-c l" . golint))
+  :init
+  (add-hook 'go-mode-hook
+	    (lambda ()
+	      (setq tab-width 4)
+	      (setq c-basic-offset 4)
+	      (setq indent-tabs-mode t)
+	      (if (not (string-match "go" compile-command))
+		  (set (make-local-variable 'compile-command)
+		       "go generate && go build -v && go test -v && go vet"))
+	      (company-mode)
+	      (go-eldoc-setup)
+	      (go-guru-hl-identifier-mode)))
+  :config
+  (use-package company
+    :ensure t)
+  (use-package company-go
+    :ensure t)
+  (use-package go-eldoc
+    :ensure t)
+  (use-package golint
+    :ensure t)
+  (use-package go-guru
+    :ensure t)
+  (setq godef-command "godef")
+  (setq gofmt-command "gofmt")
+  (setq go-guru-debug t)
+  (setq company-go-show-annotation t)
+  (flycheck-mode)
+  (setq flycheck-check-syntax-automatically t)
+  (add-to-list 'company-backends '(company-go :with company-dabbrev-code))
+  (setq company-transformers '(company-sort-by-backend-importance))
+  (add-hook 'before-save-hook 'gofmt-before-save))
