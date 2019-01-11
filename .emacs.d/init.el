eeec2dd9299f1d359493735f6fe7d108d17a8f23
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-01-11 20:00:16 +0900

    emacs: add several components

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,22 +1,5 @@
-(menu-bar-mode -1)
-(show-paren-mode 1)
-(setq show-paren-style 'mixed)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq read-file-name-completion-ignore-case t)
-(setq next-line-add-newlines nil)
-(setq make-backup-files nil)
-(setq vc-follow-syslinks t)
-(global-auto-revert-mode 1)
-
 (require 'package)
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
-
-;;;;; add melpa and orgmode for packages
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "http://melpa.org/packages/")
@@ -42,16 +25,32 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(use-package auto-complete-config
-  :ensure auto-complete
-  :init
-  (global-auto-complete-mode 1)
-  )
+(menu-bar-mode -1)
+(show-paren-mode 1)
+(setq show-paren-style 'mixed)
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq read-file-name-completion-ignore-case t)
+(setq next-line-add-newlines nil)
+(setq make-backup-files nil)
+(setq vc-follow-syslinks t)
+(global-auto-revert-mode 1)
+
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+(setq ring-bell-function 'ignore)
+
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+(setq-default indent-tabs-mode nil)
 
 (use-package solarized-theme
-;;  :custom
-;;  (solarized-use-variable-pitch nil)
-;;  (x-underline-at-descent-line t)
+  ;;  :custom
+  ;;  (solarized-use-variable-pitch nil)
+  ;;  (x-underline-at-descent-line t)
   :init
   (load-theme 'solarized-dark t))
 
@@ -88,28 +87,79 @@
   :commands
   (yas-minor-mode yas-global-mode))
 
-(use-package helm
+
+(use-package company
   :ensure t
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x f" . helm-recentf)
-         ;; ("C-SPC" . helm-dabbrev)
-         ;; ("M-y" . helm-show-kill-ring)
-         ("C-x b" . helm-buffers-list))
-  :bind (:map helm-map
-	      ("C-k" . helm-previous-line)
-	      ("C-j" . helm-next-line)
-	      ("C-p" . helm-previous-page)
-	      ("C-n" . helm-next-page)
-	      ("C-a" . helm-beginning-of-buffer)
-	      ("C-e" . helm-end-of-buffer))
-  :config (progn
-	    (setq helm-buffers-fuzzy-matching t)
-	    (use-package yaml-mode
-	      :ensure t
-	      :mode ("\\.ya?ml\\'" . yaml-mode))
-	    )
+  :config
+  (setq company-transformers '(company-sort-by-backend-importance))
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 3) 
+  (setq company-selection-wrap-around t)
+  (setq completion-ignore-case t)
+  (setq company-dabbrev-downcase nil)
+  (setq company-tooltip-align-annotations t)
+  (setq company-tooltip-flip-when-above t)
+  (define-key company-active-map (kbd "C-n") 'company-select-next) ;; C-n, C-pで補完候補を次/前の候補を選択
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-j") 'company-select-next) ;; C-n, C-pで補完候補を次/前の候補を選択
+  (define-key company-active-map (kbd "C-k") 'company-select-previous)
+  (define-key company-search-map (kbd "C-j") 'company-select-next)
+  (define-key company-search-map (kbd "C-k") 'company-select-previous)
+  (global-company-mode)
+  :ensure t
   )
+(use-package flyspell
+  :if
+  (executable-find "aspell")
+  :config
+  (setq ispell-program-name "aspell" ; use aspell instead of ispell
+        ispell-extra-args '("--sug-mode=ultra"))
+  (add-hook 'text-mode-hook #'flyspell-mode)
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-separator "/")
+  ;; rename after killing uniquified
+  (setq uniquify-after-kill-buffer-p t)
+  ;; don't muck with special buffers
+  (setq uniquify-ignore-buffers-re "^\\*"))
+
+(use-package anzu
+  :ensure t
+  :bind (("C-r" . anzu-query-replace)
+         ("C-S-r" . anzu-query-replace-regexp))
+  :config
+  (global-anzu-mode))
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+(use-package rainbow-mode
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-mode))
+
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)))
+
+(use-package git-timemachine
+  :ensure t
+  :bind (("s-g" . git-timemachine)))
+
+(use-package ruby-mode
+  :config
+  (setq ruby-insert-encoding-magic-comment nil)
+  (add-hook 'ruby-mode-hook #'subword-mode))
 
 (use-package go-mode
   :ensure t
@@ -134,46 +184,50 @@
 	      (if (not (string-match "go" compile-command))
 		  (set (make-local-variable 'compile-command)
 		       "go generate && go build -v && go test -v && go vet"))
-	      (company-mode)
-	      (go-eldoc-setup)
-	      (go-guru-hl-identifier-mode)))
+	      ;;(company-mode)
+	      ;;(go-guru-hl-identifier-mode)
+	      )
+	    )
   :config
-  (use-package company
-    :ensure t)
   (use-package company-go
-    :ensure t)
-  (use-package flycheck
-    :ensure t)
-  (use-package go-eldoc
     :ensure t)
   (use-package golint
     :ensure t)
-  (use-package go-guru
-    :ensure t)
-  (use-package lsp-go
-    :after (lsp-mode go-mode)
-    :custom (lsp-go-language-server-flags '(
-					    "-gocodecompletion"
-					    "-diagnostics"
-					    "-lint-tool=golint"))
-    :hook (go-mode . lsp-go-enable)
-    :commands lsp-go-enable)
-  (global-flycheck-mode)
-  (setq godef-command "godef")
-  (setq gofmt-command "gofmt")
-  (setq go-guru-debug t)
-  (setq company-go-show-annotation t)
-  (setq flycheck-check-syntax-automatically t)
-  (add-to-list 'company-backends '(company-go :with company-dabbrev-code))
-  (setq company-transformers '(company-sort-by-backend-importance))
-  (add-hook 'before-save-hook 'gofmt-before-save)
-  )
+  (add-hook 'before-save-hook #'gofmt-before-save))
 
-(use-package json-mode)
+(use-package lsp-go
+  :after (lsp-mode go-mode)
+  :custom (lsp-go-language-server-flags '(
+    "-gocodecompletion"
+    "-diagnostics"
+    "-lint-tool=golint"))
+  :hook (go-mode . lsp-go-enable)
+  :commands lsp-go-enable)
 
-(use-package markdown-mode)
 
-(use-package protobuf-mode)
-(use-package ruby-mode)
-(use-package toml-mode)
-(use-package yaml-mode)
+(use-package json-mode
+  :ensure t)
+
+(use-package protobuf-mode
+  :ensure t)
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package markdown-mode
+  :ensure t
+  :mode (("\\.md\\'" . gfm-mode)
+         ("\\.markdown\\'" . gfm-mode))
+  :config
+  (setq markdown-fontify-code-blocks-natively t))
+
+(use-package elixir-mode
+  :ensure t
+  :config
+  (add-hook 'elixir-mode #'subword-mode))
+
+(use-package dockerfile-mode
+  :ensure t
+  :mode "/Dockerfile\\'")
+
+;;; init.el ends here
