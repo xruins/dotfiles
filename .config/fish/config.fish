0189c1a2dc8b4cf6ca9c9f31e127e1a9cd44403b
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-02-12 19:55:01 +0900

    fish: initialize gcloud completion and path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,15 @@
 # fisherman
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    set -q XDG_CONFIG_HOME
+    or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
 
 # history search with peco
 function fish_user_key_bindings
-  bind \cr peco_select_history
-  bind \cf peco_change_directory
+    bind \cr peco_select_history
+    bind \cf peco_change_directory
 end
 
 # PATH
@@ -16,8 +17,8 @@ set -U fish_user_paths $fish_user_paths $HOME/bin
 set -U fish_user_paths $fish_user_paths $HOME/.anyenv/bin
 # anyenv
 if test -x anyenv
-   set -U fish_user_paths $fish_user_paths $HOME/.anyenv/bin
-   eval (anyenv init - | source)
+    set -U fish_user_paths $fish_user_paths $HOME/.anyenv/bin
+    eval (anyenv init - | source)
 end
 
 # golang
@@ -25,3 +26,14 @@ set -U GOPATH $HOME
 set -U fish_user_paths $fish_user_paths $GOPATH/bin
 
 set -U PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/platform/google_appengine $PATH
+
+# Google Cloud SDK
+if test -x gcloud
+    if test -d /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+        set -U GCLOUD_ROOT_PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+    else if test -d ~/google-cloud-sdk
+        set -U GCLOUD_ROOT_PATH ~/google-cloud-sdk
+    end
+    bass source "{$GCLOUD_ROOT_PATH}/completion.bash.inc"
+    source "{$GCLOUD_ROOT_PATH]/path.fish.inc"
+end
