72736f151113a1b5de5eb6d53e526fb33e172723
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-02-12 18:51:23 +0900

    replaced oh-my-fish with fisherman

--- .config/fish/functions/git_author_hook.fish
+++ .config/fish/functions/git_author_hook.fish
@@ -1,10 +1,12 @@
 function change_git_author_on_pwd_changed --description 'Switch git author base on PWD' --on-variable PWD
     if not (string match -i "*dena*" $PWD) -eq 0 # non-DeNA projects
-        set GIT_COMMITER_NAME='Ruins'
-        set EMAIL='ruinscorocoro+github@gmail.com'
+        set -U GIT_COMMITER_NAME Ruins
+        set -U EMAIL ruinscorocoro+github@gmail.com
+        set -U GITHUB_HOST=github.com
     else # DeNA projects
-        set GIT_COMMITER_NAME='Hikaru Watanabe'
-        set EMAIL='hikaru.watanabe@dena.com'
+        set -U GIT_COMMITER_NAME 'Hikaru Watanabe'
+        set -U EMAIL hikaru.watanabe@dena.com
+        set -U GITHUB_HOST=github.dena.jp
     end
-    set GIT_AUTHOR_NAME=$GIT_COMMITER_NAME
+    set -U GIT_AUTHOR_NAME $GIT_COMMITER_NAME
 end
