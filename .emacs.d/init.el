cca9a8d79a15e366a3dad1763a8c41476b478644
ruins <ruinscorocoro@gmail.com>
2019-04-05 00:28:30 +0900

    emacs: changed custom variables location

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -331,17 +331,5 @@
     '(add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
   )
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (which-key yasnippet yaml-mode use-package rainbow-mode rainbow-delimiters protobuf-mode markdown-mode magit json-mode go-mode git-gutter flycheck-popup-tip fish-mode elixir-mode dockerfile-mode anzu amx))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(show-paren-match ((nil (:background "#44475a" :foreground "#f1fa8c")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
