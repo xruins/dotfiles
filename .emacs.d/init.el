4f80755d97ba24beee76a05f4228deee0cfd17cc
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-07-26 19:31:42 +0900

    emacs: messed emacs components up

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -99,6 +99,7 @@
   :bind
   (:map go-mode-map
 	("M-." . godef-jump)
+        ("M-," . pop-tag-mark)
 	("C-c C-r" . go-remove-unused-imports)
 	("C-c i" . go-goto-imports)
         ("C-c d" . godoc)
@@ -119,7 +120,7 @@
                             (define-key company-active-map (kbd "C-s") 'company-filter-candidates) ;; C-sで絞り込む
                             (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
                             (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete) ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
-                            ))	
+                            ))
   (add-hook 'go-mode-hook
 	    (lambda ()
 	      (setq tab-width 4)
@@ -133,10 +134,15 @@
 	      )
 	    )
   :config
+  (use-package go-eldoc
+  :config
+  (add-hook 'go-mode-hook 'go-eldoc-setup))
+  (use-package flycheck-golangci-lint
+    :ensure t
+    :hook (go-mode . flycheck-golangci-lint-setup))
   (use-package company-go
     :ensure t)
-  (use-package golint
-    :ensure t)
+  (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save)
   )
 
@@ -208,79 +214,6 @@
   :ensure t
   :bind
   ("C-c m" . magit-status))
-
-;; (use-package lsp-mode
-;;   :custom
-;;   ;; debug
-;;   (lsp-print-io nil)
-;;   (lsp-trace nil)
-;;   (lsp-print-performance nil)
-;;   ;; general
-;;   (lsp-auto-guess-root t)
-;;   (lsp-document-sync-method 'incremental) ;; always send incremental document
-;;   (lsp-response-timeout 5)
-;;   (lsp-prefer-flymake 'flymake)
-;;   (lsp-enable-completion-at-point nil)
-;;   :hook
-;;   (go-mode . lsp)
-;;   :bind
-;;   (:map lsp-mode-map
-;;         ("C-c r"   . lsp-rename))
-;;   :config
-;;   (require 'lsp-clients)
-;;   ;; LSP UI tools
-;;   (use-package lsp-ui
-;;     :custom
-;;     ;; lsp-ui-doc
-;;     (lsp-ui-doc-enable t)
-;;     (lsp-ui-doc-header t)
-;;     (lsp-ui-doc-include-signature t)
-;;     (lsp-ui-doc-position 'top) ;; top, bottom, or at-point
-;;     (lsp-ui-doc-max-width 150)
-;;     (lsp-ui-doc-max-height 30)
-;;     (lsp-ui-doc-use-childframe t)
-;;     (lsp-ui-doc-use-webkit t)
-;;     ;; lsp-ui-flycheck
-;;     (lsp-ui-flycheck-enable nil)
-;;     ;; lsp-ui-sideline
-;;     (lsp-ui-sideline-enable nil)
-;;     (lsp-ui-sideline-ignore-duplicate t)
-;;     (lsp-ui-sideline-show-symbol t)
-;;     (lsp-ui-sideline-show-hover t)
-;;     (lsp-ui-sideline-show-diagnostics nil)
-;;     (lsp-ui-sideline-show-code-actions nil)
-;;     ;; lsp-ui-imenu
-;;     (lsp-ui-imenu-enable nil)
-;;     (lsp-ui-imenu-kind-position 'top)
-;;     ;; lsp-ui-peek
-;;     (lsp-ui-peek-enable t)
-;;     (lsp-ui-peek-peek-height 20)
-;;     (lsp-ui-peek-list-width 50)
-;;     (lsp-ui-peek-fontify 'on-demand) ;; never, on-demand, or always
-;;     :preface
-;;     (defun ladicle/toggle-lsp-ui-doc ()
-;;       (interactive)
-;;       (if lsp-ui-doc-mode
-;;           (progn
-;;             (lsp-ui-doc-mode -1)
-;;             (lsp-ui-doc--hide-frame))
-;;         (lsp-ui-doc-mode 1)))
-;;     :bind
-;;     (:map lsp-mode-map
-;;           ("C-c C-r" . lsp-ui-peek-find-references)
-;;           ("C-c C-j" . lsp-ui-peek-find-definitions)
-;;           ("C-c i"   . lsp-ui-peek-find-implementation)
-;;           ("C-c m"   . lsp-ui-imenu)
-;;           ("C-c s"   . lsp-ui-sideline-mode)
-;;           ("C-c d"   . ladicle/toggle-lsp-ui-doc))
-;;     :hook
-;;     (lsp-mode . lsp-ui-mode))
-;;   ;; Lsp completion
-;;   (use-package company-lsp
-;;     :custom
-;;     (company-lsp-cache-candidates t) ;; always using cache
-;;     (company-lsp-async t)
-;;     (company-lsp-enable-recompletion nil)))
 
 (use-package rainbow-delimiters
   :hook
