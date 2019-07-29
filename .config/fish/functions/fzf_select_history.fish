462ce9eec8b9fbb4f7d983e46af368089bd6504f
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-07-29 17:08:23 +0900

    fish: fixed history search

--- .config/fish/functions/fzf_select_history.fish
+++ .config/fish/functions/fzf_select_history.fish
@@ -1,7 +1,5 @@
 function fzf_select_history
-    history | fzf --query "$argv" | read fzf_select_history_result
-
-    if test $status -eq 0 ]
+    if history | fzf --query "$argv" | read fzf_select_history_result
         commandline $fzf_select_history_result
     else
         commandline ''
