f990c6d5993867d2757afea16e7bf39767b2efa5
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-09-06 15:38:21 +0900

    go-deps: add files to make easy to install executables by golang

---
+++ go-deps/deps.go
@@ -0,0 +1,8 @@
+package deps
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/motemen/ghq"
+	_ "github.com/motemen/gore/cmd/gore"
+	_ "golang.org/x/tools/cmd/goimports"
+)
---
+++ go-deps/go.mod
@@ -0,0 +1,3 @@
+module github.com/xruins/dotfiles/go-deps
+
+go 1.13
