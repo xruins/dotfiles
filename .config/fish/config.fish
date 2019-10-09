11e2769535341dc97725a338b19c1816f54f9d69
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-10-09 16:54:26 +0900

    stop to use "-U" option to prevent fish_variables gets fat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,13 +17,13 @@ function fish_user_key_bindings
 end
 
 # PATH
-set -U fish_user_paths $fish_user_paths $HOME/bin
-set -U fish_user_paths $fish_user_paths $HOME/.anyenv/bin
-set -U fish_user_paths $fish_user_paths /snap/bin
+set fish_user_paths $fish_user_paths $HOME/bin
+set fish_user_paths $fish_user_paths $HOME/.anyenv/bin
+set fish_user_paths $fish_user_paths /snap/bin
 
 # golang
-set -x -U GOPATH $HOME
-set -U fish_user_paths $fish_user_paths $GOPATH/bin
+set -x GOPATH $HOME
+set fish_user_paths $fish_user_paths $GOPATH/bin
 
 # anyenv
 if type -q anyenv
@@ -37,11 +37,11 @@ end
 
 # Google Cloud SDK
 if test -d /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
-    set -U GCLOUD_ROOT_PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
+    set GCLOUD_ROOT_PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk
 else if test -d ~/google-cloud-sdk
-    set -U GCLOUD_ROOT_PATH ~/google-cloud-sdk
+    set GCLOUD_ROOT_PATH ~/google-cloud-sdk
 else if test -d /usr/share/google-cloud-sdk/
-    set -U GCLOUD_ROOT_PATH /usr/share/google-cloud-sdk
+    set GCLOUD_ROOT_PATH /usr/share/google-cloud-sdk
 end
 if test -d $GCLOUD_ROOT_PATH/platform/google_appengine
     set -u fish_user_paths $fish_user_paths $GCLOUD_ROOT_PATH/platform/google_appengine
